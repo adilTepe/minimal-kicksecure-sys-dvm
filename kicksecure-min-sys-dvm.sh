97f5181aed3a94f34185b9dae3210496b876b1cc
adilTepe <104206649+adilTepe@users.noreply.github.com>
2023-10-09 03:52:52 +0200

    Update kicksecure-min-sys-dvm.sh

--- kicksecure-min-sys-dvm.sh
+++ kicksecure-min-sys-dvm.sh
@@ -9,7 +9,7 @@ qvm-template install $SOURCE_TEMPLATE
 # update repos and upgrade packages
 qvm-run --pass-io -u root $SOURCE_TEMPLATE "apt-get update && apt-get full-upgrade -y"
 # install basic packages
-< pulseaudio-qubes"
+qvm-run --pass-io -u root $SOURCE_TEMPLATE "apt-get install -y qubes-core-agent-passwordless-root qubes-app-shutdown-idle qubes-core-agent-nautilus nautilus xfce4-terminal qubes-menus pulseaudio-qubes"
 # set Xfce4-terminal as default
 qvm-run --pass-io -u root $SOURCE_TEMPLATE "update-alternatives --set x-terminal-emulator /usr/bin/xfce4-terminal.wrapper"
 # shutdown
@@ -35,22 +35,22 @@ qvm-run --pass-io --no-gui --user=user $TARGET_TEMPLATE 'sudo http_proxy=http://
 qvm-run --pass-io --no-gui --user=user $TARGET_TEMPLATE 'sudo apt-get update'
 qvm-run --pass-io --no-gui --user=user $TARGET_TEMPLATE 'sudo apt-get dist-upgrade -y'
 qvm-run --pass-io --no-gui --user=user $TARGET_TEMPLATE 'sudo apt-get install -y --no-install-recommends kicksecure-qubes-gui'
+# kicksecure post-install
 qvm-run --pass-io --no-gui --user=user $TARGET_TEMPLATE 'sudo extrepo disable kicksecure'
 qvm-run --pass-io --no-gui --user=user $TARGET_TEMPLATE 'sudo mv /etc/apt/sources.list ~/'
 qvm-run --pass-io --no-gui --user=user $TARGET_TEMPLATE 'sudo touch /etc/apt/sources.list'
+# enable kicksecure repo to stay up-to-date
 qvm-run --pass-io --no-gui --user=user $TARGET_TEMPLATE 'sudo repository-dist --enable --repository stable-proposed-updates'
-
+# A bit more hardenning
 qvm-run --pass-io --no-gui --user=root $TARGET_TEMPLATE 'apt-get install -y lkrg-dkms tirdad apparmor-notify apparmor-profile-everything libpam-apparmor notification-daemon python3-notify2'
 qvm-prefs -s $TARGET_TEMPLATE kernelopts "apparmor=1 security=apparmor"
+# install the packages required to enable the minimal template function as a sys qube
 qvm-run --pass-io --no-gui --user=root $TARGET_TEMPLATE "apt-get install -y --no-install-recommends qubes-core-agent-networking wpasupplicant qubes-core-agent-network-manager firmware-iwlwifi qubes-usb-proxy qubes-input-proxy-sender zenity policykit-1 libblockdev-crypto2 ntfs-3g qubes-core-agent-dom0-updates"
-qvm-shutdown --wait $TARGET_TEMPLATE
-sleep 20
-
-qvm-run --pass-io --no-gui --user=root $TARGET_TEMPLATE "modprobe -r iwlwifi ; modprobe iwlwifi"
 
 qvm-shutdown --wait $TARGET_TEMPLATE
 sleep 20
 
+# create a disposable template
 qvm-create --template $TARGET_TEMPLATE --label red $TARGET_TEMPLATE_DISP 
 qvm-prefs $TARGET_TEMPLATE_DISP template_for_dispvms True
 qvm-features $TARGET_TEMPLATE_DISP appmenus-dispvm 1  
